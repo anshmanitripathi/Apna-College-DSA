7e03b5f150faa524929eda4d299734195afa34c6
Ansh Mani Tripathi <143960008+anshmanitripathi@users.noreply.github.com>
2024-09-03 23:56:35 +0530

    first commit

---
+++ 1. Variables & DataTypes/javabasics.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class javabasics{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("* * * *");
+        System.out.println("* * * ");
+        System.out.println("* * ");
+        System.out.println("* ");
+
+        int a = 10;
+        int b= 4;
+        int sum = a+b;
+        System.out.println("Sum = " + sum);
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String : ");
+        String ab = sc.nextLine();
+        System.out.println("Enter boolean : ");
+        boolean ans = sc.nextBoolean();
+        System.out.println("Enter int : ");
+        int an = sc.nextInt();
+       
+
+
+        sc.close();
+    }
+}
