dfebf36a85cf5774a1d1d715a01f041aa18e37e3
Ansh Mani Tripathi <143960008+anshmanitripathi@users.noreply.github.com>
2024-09-05 01:17:37 +0530

    java

---
+++ 1. Variables & DataTypes/Area.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Area {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter radius of circle : ");
+        int radius = sc.nextInt();
+        float Area = 3.14f * radius *radius;
+        System.out.println("Area = "+ Area);
+
+        int $ = 24;
+        System.out.println($);
+
+    }
+}
