e4852179ffaf6b1c65f0106f4a8dbfdf3f292a9a
Ansh Mani Tripathi <143960008+anshmanitripathi@users.noreply.github.com>
2024-11-21 16:08:03 +0530

    java

--- 13. Recursion/c_factorial.java
+++ 13. Recursion/c_factorial.java
@@ -1,14 +1,14 @@
 public class c_factorial {
-    public static int fact(int n){
+    public static int facto(int n){
         if(n==0){
             return 1;
         }
-        int fn = n * fact(n-1);
+        int fn = n * facto(n-1);
 
         return fn;
     }
 
     public static void main(String[] args) {
-        fact(5);
+        facto(5);
     }
 }
