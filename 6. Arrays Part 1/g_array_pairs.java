998f8e121ca3b66ebbb268f14b79002c9e35768c
Ansh Mani Tripathi <143960008+anshmanitripathi@users.noreply.github.com>
2025-04-28 15:38:12 +0530

    Arrays

--- 6. Arrays Part 1/g_array_pairs.java
+++ 6. Arrays Part 1/g_array_pairs.java
@@ -3,8 +3,9 @@ public class g_array_pairs {
     public static void pairs(int[] arr){
         for(int i=0; i<arr.length; i++){
             for(int j=i+1; j<arr.length; j++){
-                System.out.println(" ("+arr[i]+","+arr[j]+")");
+                System.out.print(" ("+arr[i]+","+arr[j]+")");
             }
+            System.out.println();
         }
     }
 
